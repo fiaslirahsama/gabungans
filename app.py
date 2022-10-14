589530fffdf35307235d24f8ac2ef6f135b0005e
fiaslirahsama <fiaslirahsama@gmail.com>
2022-10-14 09:43:49 +0700

    [UPDATE} multiapp satu jam saja kutelah bisa

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from werkzeug.middleware.dispatcher import DispatcherMiddleware # use to combine each Flask app into a larger one that is dispatched based on prefix
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
 from app_perpus import create_app_perpus 
 from app_hospital import create_app_hospital 
 
--- run.py
+++ run.py
@@ -1,22 +1,22 @@
 from werkzeug.serving import run_simple # werkzeug development server
-from app_perpus import create_app_perpus
-from app_hospital import create_app_hospital
+# from app_perpus import create_app_perpus
+# from app_hospital import create_app_hospital
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from app import multiapp
+from app import multiapp, app, app_perpus
 
 
 dblo = SQLAlchemy()
 migrain = Migrate()
 migroin = Migrate()
-app2 = create_app_perpus()
-print(app2)
-app = create_app_hospital()
-print(app)
+# app2 = create_app_perpus()
+# print(app2)
+# app = create_app_hospital()
+# print(app)
 migrain.init_app(app, dblo)
 migrain.app = app
-migroin.init_app(app2, dblo)
-migroin.app = app2
+migroin.init_app(app_perpus, dblo)
+migroin.app = app_perpus
 
 if __name__ == '__main__':
     # app2.run('localhost', 5001, threaded=True)
